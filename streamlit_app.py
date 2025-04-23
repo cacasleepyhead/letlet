27aa9eb236c532ec3ef698b30132598922e23c85
cacasleepyhead <khansa.zaf09@gmail.com>
2025-04-23 15:28:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("🎈 hai barudawgh")
 st.write(
-    "kayek lucu kata aleta."
+    "kayek noyek lucu kata aleta."
 )
 st.header("noycayek")
 st.image("IMG_0502.jpeg")
